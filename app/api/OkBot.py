6d2bf6e13f6c4ea258916be7518e2af9cbdc968c
prometheus <taskevichoff@gmail.com>
2022-12-02 15:54:29 +0500

    bot

--- app/api/OkBot.py
+++ app/api/OkBot.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Bot():
+class Bot:
     def __init__(self, login, password) -> None:
         self.__base_url = 'https://ok.ru/'
         self.__login = login
